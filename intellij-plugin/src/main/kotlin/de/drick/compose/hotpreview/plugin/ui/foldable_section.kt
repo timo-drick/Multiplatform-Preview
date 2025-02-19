a912cc4077748acc3b682a621cbe601e10215683
Timo Drick <timo.drick@7p-group.com>
2025-02-19 15:18:59 +0100

    Added foldable sections

---
+++ intellij-plugin/src/main/kotlin/de/drick/compose/hotpreview/plugin/ui/foldable_section.kt
@@ -0,0 +1,93 @@
+package de.drick.compose.hotpreview.plugin.ui
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import de.drick.compose.hotpreview.HotPreview
+import org.jetbrains.jewel.foundation.theme.JewelTheme
+import org.jetbrains.jewel.ui.Orientation
+import org.jetbrains.jewel.ui.component.Divider
+import org.jetbrains.jewel.ui.component.Icon
+import org.jetbrains.jewel.ui.component.Text
+import org.jetbrains.jewel.ui.component.styling.LocalGroupHeaderStyle
+import org.jetbrains.jewel.ui.icons.AllIconsKeys
+import org.jetbrains.jewel.ui.theme.dividerStyle
+
+
+@HotPreview(widthDp = 300)
+@HotPreview(widthDp = 300, darkMode = false)
+@Composable
+private fun PreviewConfigSection() {
+    SelfPreviewTheme {
+        ConfigSection(modifier = Modifier, label = "Test Section", isInitiallyFolded = false) {
+            Text("Config value")
+        }
+    }
+}
+@HotPreview(widthDp = 300)
+@HotPreview(widthDp = 300, darkMode = false)
+@Composable
+private fun PreviewConfigSectionFolded() {
+    SelfPreviewTheme {
+        ConfigSection(modifier = Modifier, label = "Test Section", isInitiallyFolded = true) {
+            Text("Config value")
+        }
+    }
+}
+
+@Composable
+fun ConfigSection(
+    label: String,
+    isInitiallyFolded: Boolean = false,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    var showSection by remember { mutableStateOf(isInitiallyFolded.not()) }
+    val animatedRotation = animateFloatAsState(if (showSection) 0f else -90f)
+    val style = LocalGroupHeaderStyle.current
+
+    Column(modifier) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth().clickable { showSection = showSection.not() },
+        ) {
+            Icon(
+                modifier = Modifier.rotate(animatedRotation.value),
+                key = AllIconsKeys.General.ArrowDown,
+                contentDescription = "Fold"
+            )
+            //Divider(Orientation.Horizontal, Modifier.width(8.dp), thickness = 2.dp)
+            Text(
+                text = label,
+                modifier = Modifier.padding(horizontal = 4.dp),
+            )
+            //Divider(Orientation.Horizontal, Modifier.weight(1f), thickness = 2.dp)
+        }
+        AnimatedVisibility(
+            visible = showSection,
+        ) {
+            Row(Modifier.height(IntrinsicSize.Min)) {
+                Divider(
+                    Orientation.Vertical,
+                    modifier = Modifier.fillMaxHeight().padding(horizontal = 8.dp),
+                    color = style.colors.divider,
+                    thickness = style.metrics.dividerThickness,
+                )
+                Box(Modifier.weight(1f).padding(vertical = 8.dp)) {
+                    content()
+                }
+            }
+        }
+    }
+}
