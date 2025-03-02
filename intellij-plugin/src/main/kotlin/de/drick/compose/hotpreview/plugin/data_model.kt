2b588af3f7e3be63a2827a03b6f5fe7a5d6ef323
Timo Drick <t.drick@apps-on-air.de>
2025-03-02 15:11:35 +0100

    Set default density to 1f

--- intellij-plugin/src/main/kotlin/de/drick/compose/hotpreview/plugin/data_model.kt
+++ intellij-plugin/src/main/kotlin/de/drick/compose/hotpreview/plugin/data_model.kt
@@ -22,6 +22,6 @@ data class HotPreviewModel(
     val heightDp: Int = -1,    // if < 0 it will adapt to content max 1024 dp
     val locale: String = "",   // Not supported yet!
     val fontScale: Float = 1f, // The scaling factor for fonts. Should be between 0.5f and 2.0f
-    val density: Float = 2f,   // The logical density of the display. This is a scaling factor for the Dp unit.
+    val density: Float = 1f,   // The logical density of the display. This is a scaling factor for the Dp unit.
     val darkMode: Boolean = true,
 )
